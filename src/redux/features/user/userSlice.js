00ee1ec71054407b5572d64d52ba3079e2487b8b
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-04-22 00:16:16 +0600

    set the user with firebase implemented

--- src/redux/features/user/userSlice.js
+++ src/redux/features/user/userSlice.js
@@ -1,7 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import auth from "../../../utils/firebase.config";
-
 
 const initialState = {
   name: "",
@@ -12,39 +11,54 @@ const initialState = {
 };
 export const createUser = createAsyncThunk(
   "userSlice/createUser",
-  async ({email , password}) => {
-  const data = await  createUserWithEmailAndPassword(auth , email, password)
-  console.log(data)
-  return;
+  async ({ email, password, name }) => {
+    const data = await createUserWithEmailAndPassword(auth, email, password);
+    await updateProfile(auth.currentUser, {
+      displayName: name,
+    });
+    console.log(data);
+    return {
+      email: data?.user.email,
+      name: data?.user.displayName,
+    };
   }
 );
 const userSlice = createSlice({
   name: "userSlice",
   initialState,
-  reducers: {},
+  reducers: {
+    setUser: (state, { payload }) => {
+      (state.email = payload.email), (state.name = payload.name);
+    },
+    toggleLoading: (state, { payload }) => {
+      state.isLoading = payload;
+    },
+  },
   extraReducers: (builder) => {
     builder.addCase(createUser.pending, (state) => {
-      state.name = "",
-      state.email = "",
-      state.isLoading = true,
-      state.isError = false,
-      state.error = ""
-    })
-    builder.addCase(createUser.fulfilled, (state,{payload}) => {
-      state.name = payload.name,
-      state.email = payload.email,
-      state.isLoading = false,
-      state.isError = false,
-      state.error = ""
-    })
-    builder.addCase(createUser.rejected, (state , action) => {
-      state.name = "",
-      state.email = "",
-      state.isLoading = false,
-      state.isError = true,
-      state.error = action.error.message
-    })
-  }
+      (state.name = ""),
+        (state.email = ""),
+        (state.isLoading = true),
+        (state.isError = false),
+        (state.error = "");
+    });
+    builder.addCase(createUser.fulfilled, (state, { payload }) => {
+      (state.name = payload.name),
+        (state.email = payload.email),
+        (state.isLoading = false),
+        (state.isError = false),
+        (state.error = "");
+    });
+    builder.addCase(createUser.rejected, (state, action) => {
+      (state.name = ""),
+        (state.email = ""),
+        (state.isLoading = false),
+        (state.isError = true),
+        (state.error = action.error.message);
+    });
+  },
 });
+
+export const { setUser, toggleLoading } = userSlice.actions;
 
 export default userSlice.reducer;
